788858d5ec1d87fb80ede1632d1d277e70768ee5
Jotisman Banerjee <127194267+jotisman01@users.noreply.github.com>
2023-09-02 09:09:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Navbar from './component/Navbar';
+import Body from './component/Body';
+import Icons from './component/Icons';
+
+function App(){
+  return (
+<>
+ <Navbar/>
+ <Icons/>
+ <Body/>
+ </>
+  );
+}
+
+export default App;
